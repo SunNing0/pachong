79a761afc834dbc04f04fc8d61114ef6d137d615
SunNing0 <823909338@qq.com>
2017-02-16 09:54:41 +0800

    能输出中文，能匹配单一内容

--- pachong2.py
+++ pachong2.py
@@ -1,24 +1,24 @@
 
-# _*_ coding:utf-8 _*_
+# encoding:utf-8 
 import urllib
 import urllib2
 import re
+import json
 
-page = 1
-#url = 'http://www.qiushibaike.com/hot/page/'+str(page)
+url = 'http://www.cnblogs.com/allenblogs/archive/2010/09/13/1824842.html'
 user_agent = 'Mozilla/4.0(compatible;MSIE 5.5;Windows NT)'
-url = 'http://www.qiushibaike.com/hot/'
 headers = {'User-Agent':user_agent}
 try:
     request = urllib2.Request(url,headers = headers)
     response = urllib2.urlopen(request)
     content = response.read().decode('utf-8')
     #print content
-    pattern = re.compile('id="(.*?)"class',re.S)
+    pattern = re.compile('<title>(.*?)</title>',re.S)
     items = re.findall(pattern,content)
-    print items
+ #   print type(items)
+    print json.dumps(items,encoding="UTF-8",ensure_ascii=False)
     for item in items:
-        print item[0]
+        print item[0]  
 except urllib2.URLError,e:
     if hasattr(e,"code"):
         print e.code
